5ad87744b2987add3a3f0ee41b25749cc7121fd9
vola-g <vola.graholska@gmail.com>
2019-03-16 17:05:06 +0300

    add solution for love-triangle task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = 
+function getLoveTrianglesCount(preferences = []) {
+  let loveTriangles = 0;
+  for ( i = 0; i < preferences.length; i++){
+      let firstLove = preferences[i];
+      let secondLove = preferences[firstLove - 1];
+      let thirdLove = preferences[secondLove - 1];
+      if (thirdLove === (i+1)) {
+        loveTriangles++;
+      }
+  }
+  return Math.floor(loveTriangles/3);
 };
+
+// console.log(getLoveTrianglesCount([2, 3, 1, 5, 6, 4, 8, 9, 7, 11, 12, 10, 13, 9, 13, 2, 15, 13, 12, 11]));
